d104d3a9cdd75e8ec65bd7a0f4348f437ab3c726
zooxop <zooxop@gmail.com>
2020-10-11 23:04:55 +0900

    problem 10171 commit

---
+++ BaekJoon/src/baekjoonAlgorithmStudy/Problem_10171.java
@@ -0,0 +1,19 @@
+package baekjoonAlgorithmStudy;
+
+public class Problem_10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		
+		//\    /\
+		// )  ( ')
+		//(  /  )
+		// \(__)|
+	}
+
+}
